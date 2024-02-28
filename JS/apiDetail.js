b6daf942dfbd50b000ff3126782c6be0654f15f1
SFaucher26 <158201877+SFaucher26@users.noreply.github.com>
2024-02-28 16:21:28 +0100

    Page details (#19)
    * renvoie sur une page détails au clic sur la card
    
    * fin de tache page détails

--- JS/apiDetail.js
+++ JS/apiDetail.js
@@ -1,9 +1,15 @@
-import { findMovie } from "./apiFetch.js";
-import { createMovieCard } from "./movieTemplate.js";
+import { findMovie, findVideo } from "./apiFetch.js";
+import { createMovieCard, createMovieDetails } from "./movieTemplate.js";
 
 const id_movie = new URL(document.location.href).searchParams.get("film");
 
 findMovie(id_movie).then((movie) => {
   console.log(movie);
-  createMovieCard(movie, "main");
+  findVideo(id_movie).then( (videos) => {
+    console.log(videos); 
+    createMovieDetails(movie, videos, "main");
 });
+  
+});
+
+
--- JS/movieTemplate.js
+++ JS/movieTemplate.js
@@ -18,10 +18,46 @@ export function createMovieCard(movie, dest) {
   //date sortie
   const dateSortie = createElement("p", "date-sortie", card);
   dateSortie.innerHTML = "Date de sortie : " + movie.release_date;
-  // console.log(movie.title)
-  // console.log(movie.overview)
-  // console.log(movie.poster_path)
-  // console.log(movie.id);
+
+  return card;
+}
+export function createMovieDetails(movie, videos, dest) {
+  const video = videos.results[0];
+
+  //je crée l'élément card qui affichera chaque film
+  const card = createElement("div", "container-card", dest);
+
+  //Tag line
+  const tagline = createElement("h1", "tag", card);
+  tagline.innerHTML = movie.tagline;
+
+  // genre
+  const genre = createElement("p", "genre", card);
+  const genres = movie.genres.map((e) => {
+    return e.name;
+  });
+  genre.innerHTML = genres.join(" - ");
+
+  //img
+  const img = createElement("img", "affiche", card);
+  img.url = `https://image.tmdb.org/t/p/w500/`;
+  img.src = img.url + movie.poster_path;
+  //description
+  const p = createElement("p", "description", card);
+  p.innerHTML = movie.overview;
+  //note
+  const note = createElement("p", "note", card);
+  note.innerHTML = "Note moyenne : " + Math.ceil(movie.vote_average);
+  //date sortie
+  const dateSortie = createElement("p", "date-sortie", card);
+  dateSortie.innerHTML = "Date de sortie : " + movie.release_date;
+
+  //video
+  if (video) {
+    const videoEl = createElement("iframe", "video", card);
+    videoEl.outerHTML = `<iframe width="1097" height="617" src="https://www.youtube.com/embed/${video.key}" title="${video.name}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+  }
+  return card;
 }
 
 /**
@@ -44,7 +80,6 @@ export function createElement(elem, name, location) {
     // Si c'est une string
     inDocHtml = document.querySelector(location);
   }
-
   inDocHtml.appendChild(element);
   return element;
 }
