ee6deb09c62e738011a2b72a582971cf12d3ec0a
morganb <morgan.bleunven@maif.fr>
2024-02-28 16:07:27 +0100

    filtrage termine

--- JS/comApiSectionSlider.js
+++ JS/comApiSectionSlider.js
@@ -4,36 +4,11 @@ let inputBtnNext;
 let inputBtnPrev;
 let imgSlider;
 let titleSlider;
-let checkboxes;
+let checkboxes = document.querySelectorAll(".checkbox");
+
 // let filmDatas;
 let indexFilm;
-/*================================================ */
-
-window.addEventListener("load", async () => {
-  inputBtnNext = document.querySelector(".input-next");
-  inputBtnPrev = document.querySelector(".input-prev");
-  imgSlider = document.querySelector(".img-slider");
-  titleSlider = document.querySelector(".slider-title");
-  checkboxes = document.querySelectorAll(".checkbox");
-  const movies = await fetchData();
-  addListeners(movies);
-  indexFilm = 0; // initialisation indexFilm pour le slider
-  checkboxes[2].checked = true;
-});
-
-
-/*=========================END POINT listes de films==================== */
-const apiUrl1 = 'https://api.themoviedb.org/3/discover/movie';
-/*END POINT listes des genres */
-const apiUrl2 = 'https://api.themoviedb.org/3/genre/movie/list';
-/*END POINT latest, get the newest movies */
-const apiUrl3 = 'https://api.themoviedb.org/3/movie';
-/*URL DES IMAGES WIDTH 500 */
-// const baseImgUrl = 'https://image.tmdb.org/t/p/w500'
-/*URL DES IMAGES ORIGINAL */
-const baseImgUrl = 'https://image.tmdb.org/t/p/original'
-
-/*=========Options de filtrage de l'API injectées en url: ================*/
+let movies;
 /*========= Playing now chargé par défault au chargement ================*/
 let releaseDate = '2024-02-27'
 
@@ -49,17 +24,46 @@ const queryParams = {
   // 'primary_release_date.lte' : '2024-01-15',
   // 'year' : 2023
 };
-const queryString = new URLSearchParams(queryParams).toString();
-const url = apiUrl3 + '/now_playing' + '?' + queryString;
-// const url ='https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1'
-console.log(url);
+
+/*================================================ */
+
+window.addEventListener("load", async () => {
+  inputBtnNext = document.querySelector(".input-next");
+  inputBtnPrev = document.querySelector(".input-prev");
+  imgSlider = document.querySelector(".img-slider");
+  titleSlider = document.querySelector(".slider-title");
+  // checkboxes = document.querySelectorAll(".checkbox");
+  movies = await fetchData(queryParams);
+  addListeners();
+  indexFilm = 0; // initialisation indexFilm pour le slider
+  checkboxes[1].checked = true;
+});
+
+
+/*=========================END POINT listes de films==================== */
+const apiUrl1 = 'https://api.themoviedb.org/3/discover/movie';
+/*END POINT listes des genres */
+const apiUrl2 = 'https://api.themoviedb.org/3/genre/movie/list';
+/*END POINT latest, get the newest movies */
+const apiUrl3 = 'https://api.themoviedb.org/3/movie';
+/*URL DES IMAGES WIDTH 500 */
+// const baseImgUrl = 'https://image.tmdb.org/t/p/w500'
+/*URL DES IMAGES ORIGINAL */
+const baseImgUrl = 'https://image.tmdb.org/t/p/original'
+
+/*=========Options de filtrage de l'API injectées en url: ================*/
+const createUrl = (queryParams, apiEndPoint) => {
+  const queryString = new URLSearchParams(queryParams).toString();
+  const url = apiEndPoint + '?' + queryString;
+  // console.log('Fonction CreateUrl: ', url, queryParams);
+  return(url);
+}
 /*======================================================================== */
 
 /*=============================FETCH =============================*/
-const fetchData = async () => {
+const fetchData = async (queryParams) => {
   return await listMovies(queryParams)
     .then((filmDatas) => {
-      console.log(filmDatas);
       setCardSlider(filmDatas, 0);
       return filmDatas;
     })
@@ -75,56 +79,77 @@ const setCardSlider = (data, indexFilm) => {
 };
 /*================================================ */
 
-function addListeners(filmDatas) {
+function addListeners() {
   inputBtnNext.addEventListener("click", () => {
-    // console.log("Log bouton next: ",filmDatas);
     if (indexFilm != 19) {
       indexFilm++;
     } else {
       indexFilm = 0;
     }
 
-    setCardSlider(filmDatas, indexFilm);
+    setCardSlider(movies, indexFilm);
   });
 
   inputBtnPrev.addEventListener("click", () => {
-    // console.log("Log bouton next: ",filmDatas);
     if (indexFilm == 0) {
-      indexFilm = filmDatas.length - 1;
+      indexFilm = movies.length - 1;
     } else {
       indexFilm--;
     }
-    setCardSlider(filmDatas, indexFilm);
+    setCardSlider(movies, indexFilm);
   });
+}
 
   //Checkbox Options de Filtrage 
   checkboxes.forEach(element => {
-    element.addEventListener('change', (e) => {
-      console.log(e.target.id);
+    
+    element.addEventListener('change', async (e) => {
+      const cb3 = document.querySelector('.cb3-in-div');
       if (e.target.id === 'choix1') {
+        if(!cb3.classList.contains('inactive')){
+          cb3.classList.add('inactive');
+        }
         const queryParams = {
+          'include_adult' : 'false',
+          'include_video' : 'false',
           'language': 'en-US',
           'page': 1,
-        };
-        // console.log(`Case cochée: ${e.target.id}`,' Query Param: ',queryParams);
+          'sort_by': 'primary_release_date.desc',
+          'primary_release_date.lte': '2024/02/27'
+      };
+      const url = createUrl(queryParams,apiUrl1);
+      movies = await fetchData(queryParams);
+        
       } else if (e.target.id === 'choix2') {
+        if(!cb3.classList.contains('inactive')){
+          cb3.classList.add('inactive');
+        }
         const queryParams = {
+          'include_adult' : 'false',
+          'include_video' : 'false',
           'language': 'en-US',
           'page': 1,
+          'sort_by' : 'popularity.desc'
         };
-        // console.log(`Case cochée: ${e.target.id}`,' Query Param: ',queryParams);
+        const url = createUrl(queryParams, apiUrl1);
+
+        movies = await fetchData(queryParams);
 
       } else {
-        document.querySelector('.cb3-in-div').classList.remove('inactive');
-        const queryParams = {
-          'language': 'en-US',
-          'year': 2023,
-          'page': 1
-        };
-        console.log(`Case cochée: ${e.target.id}`, ' Query Param: ', queryParams);
-
+        cb3.classList.remove('inactive');
+        const cb3Input = document.querySelector('.cb3-input');
+        const cb3Submit = document.querySelector('.cb3-submit');
+        cb3Submit.addEventListener('click', async () => {
+          const valueYear = cb3Input.value;
+          const queryParams = {
+            'language': 'en-US',
+            'primary_release_date.lte': `${valueYear}/12/30`,
+            'primary_release_date.gte': `${valueYear}/01/01`,
+            'page': 1
+          };
+          const url = createUrl(queryParams,apiUrl1);
+          movies = await fetchData(queryParams);
+        } )
       }
     })
   })
-
-}
