c971a77397c6606b0cf44b0abc529a0d9042d47f
SFaucher26 <sandrine.faucher@maif.fr>
2024-02-28 12:45:53 +0100

    renvoie sur une page détails au clic sur la card

--- JS/apiDetail.js
+++ JS/apiDetail.js
@@ -1,9 +1,14 @@
-import { findMovie } from "./apiFetch.js";
-import { createMovieCard } from "./movieTemplate.js";
+import { findMovie, findVideo } from "./apiFetch.js";
+import { createElement, createMovieCard, createMovieDetails } from "./movieTemplate.js";
 
 const id_movie = new URL(document.location.href).searchParams.get("film");
 
 findMovie(id_movie).then((movie) => {
   console.log(movie);
-  createMovieCard(movie, "main");
+  createMovieDetails(movie, "main");
+});
+
+findVideo(id_movie).then( (videos) => {
+    createElement()
+    console.log(videos); 
 });
--- JS/movieTemplate.js
+++ JS/movieTemplate.js
@@ -18,10 +18,38 @@ export function createMovieCard(movie, dest) {
   //date sortie
   const dateSortie = createElement("p", "date-sortie", card);
   dateSortie.innerHTML = "Date de sortie : " + movie.release_date;
-  // console.log(movie.title)
-  // console.log(movie.overview)
-  // console.log(movie.poster_path)
-  // console.log(movie.id);
+
+  return card;
+
+}
+export function createMovieDetails(movie, dest) {
+ //je crée l'élément card qui affichera chaque film
+ const card = createElement("div", "container-card", dest);
+
+ //titre
+ const h3 = createElement("h3", "titre", card);
+ h3.innerHTML = movie.title;
+ //img
+ const img = createElement("img", "affiche", card);
+ img.url = `https://image.tmdb.org/t/p/w500/`;
+ img.src = img.url + movie.poster_path;
+ //description
+ const p = createElement("p", "description", card);
+ p.innerHTML = movie.overview;
+ //note
+ const note = createElement("p", "note", card);
+ note.innerHTML = "Note moyenne : " + Math.ceil(movie.vote_average);
+ //date sortie
+ const dateSortie = createElement("p", "date-sortie", card);
+ dateSortie.innerHTML = "Date de sortie : " + movie.release_date;
+
+ //video
+ const video = createElement('iframe', 'video', card);
+ video.allow ="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+ video.allowfullscreen ;
+
+ return card;
+
 }
 
 /**
